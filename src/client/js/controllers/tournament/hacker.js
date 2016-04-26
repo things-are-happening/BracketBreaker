df950827fbfe56bc7d8a6e08c6d84fbb09e42c8f
Joseph Nolan <josephn0624@gmail.com>
2016-04-26 09:53:40 -0500

    daniel pushes toy problems

--- src/client/js/controllers/tournament/hacker.js
+++
@@ -1,9 +0,0 @@
-// Zebulan has worked hard to write all his python code in strict compliance to PEP8 rules. In this kata, you are a mischevious hacker that has set out to sabatoge all his good code.
-
-// Your job is to take PEP8 compatible function names and convert them to camelCase. For example:
-
-// zebulansNightmare('camel_case') == 'camelCase'
-// zebulansNightmare('zebulans_nightmare') == 'zebulansNightmare'
-// zebulansNightmare('get_string') == 'getString'
-// zebulansNightmare('convert_to_uppercase') == 'convertToUppercase'
-// zebulansNightmare('main') == 'main'
