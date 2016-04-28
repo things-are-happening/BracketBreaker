8e35b4207eb4cf79fc16ff7fb028ae9727d25fbc
Joseph Nolan <josephn0624@gmail.com>
2016-04-28 16:30:23 -0500

    dude

--- src/client/js/controllers/tournament/viewTournamentCtrl.js
+++ src/client/js/controllers/tournament/viewTournamentCtrl.js
@@ -1,6 +1,4 @@
 app.controller("viewTournamentCtrl", function($scope, currentTourney){
 	$scope.tournament = currentTourney.data
-	$scope.matches = currentTourney.data.teamName;
-
-	console.log($scope.matches)
+	
 })
