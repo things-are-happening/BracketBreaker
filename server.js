8cf7e304785cd30860b8ed594e83901438dafdc7
Joseph Nolan <josephn0624@gmail.com>
2016-05-01 13:32:11 -0500

    sure

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ var express = require('express'),
 	cors = require('cors'),
 	mongoose = require('mongoose'),
 	morgan = require('morgan'),
-	LocalStrategy = require('passport-local').Strategy;
 	LocalStrategy = require('passport-local').Strategy,
 	port = 9000,
-	mongoUri = 'mongodb://localhost:27017/tournament';
+	mlabs = require('./src/server/config/database.js');
+
+mongoose.connect(mlabs.url);
 
 ////// FILES //////
 var tournament = require('./src/server/controllers/tournamentCtrl');
@@ -17,7 +18,6 @@ var team = require('./src/server/controllers/teamCtrl');
 
 var passport = require('./src/server/config/passport.js');
 var userCtrl = require('./src/server/controllers/userCtrl.js');
-var passport = require('./src/server/config/passport.js');
 
 
 app.use(morgan('dev'));
@@ -68,7 +68,6 @@ app.get('/api/tournament/:id', tournament.getOne);
 app.get('/api/tournament', tournament.get);
 app.post('/api/tournament', tournament.post);
 app.put('/api/tournament', tournament.put);
-app.delete('/api/tournament/:id', tournament.delete);
 //////
 app.get('/api/match', match.getAll);
 app.get('/api/match:id', match.getOne);
@@ -88,6 +87,6 @@ app.listen(port, function() {
 });
 
 mongoose.connection.once('open', function() {
-	console.log('Connected to MongoDB at ' + mongoUri);
+	// console.log('Connected to MongoDB at ' + mongoUri);
 	console.log('Connected to MongoDB at ' + mlabs);
 });
